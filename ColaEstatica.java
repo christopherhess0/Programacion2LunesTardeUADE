289c37b3e96da98b1b092a3e00e877ac753a88e6
Lichi3006 <129699825+Lichi3006@users.noreply.github.com>
2026-03-16 15:03:15 -0300

    acolar y desacolar

--- ColaEstatica.java
+++ ColaEstatica.java
@@ -5,12 +5,14 @@ public class ColaEstatica implements ColaTDA {
         // 
     };
     public void acolar(int x){
-
-        //
+        """
+        Se recibe un un número, se lo agrega a la lista en el la última posición en la lista (o en la primera si está vacía)
+        """
     };
     public void desacolar(){
-
-        //
+        """ 
+        Se debe eliminar el ultimo valor ingresado, por lo que se obtiene el numero de la cantidad de índices y se le resta 1 para obtener el índice que se eliminará 
+        """
     };
     public int primero(){
         // Agarra en la cola el valor que tenga en la posicion 0. Y  lo muestra
